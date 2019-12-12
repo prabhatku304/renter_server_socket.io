c758e43ebaadfcb94412e47503b6dd9f47527162
b117014 <b117014@iiit-bh.ac.in>
2019-12-12 23:07:14 +0530

    modify on get router of message

--- Route/message.js
+++ Route/message.js
@@ -4,5 +4,15 @@ const router =  express.Router();
 
 
 
-router.get
+router.get('/messages',async (req,res,next)=>{
+            try{
+                  let fullMessage = await db.Message.find({});
+                  res.send(fullMessage);
+                  return next();
+            }catch(err){
+                return next(err);
+            }
+})
+
+router
 
